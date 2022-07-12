d95f70e9f8a2413a6dcbca107c63da8791162322
JeongGeon Oh <jeoh0605@gmail.com>
2022-07-12 12:24:41 +0900

    basic structure

---
+++ view/footerView.php
@@ -0,0 +1,13 @@
+<footer>
+    <img src="./public/images/brand/logo.jpg" alt="verve qlogo">
+    <ul id="footer-menu">
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Create Your Plan</li>
+    </ul>
+    <ul>
+        <li><i class="fa-brands fa-facebook-square"></i></li>
+        <li><i class="fa-brands fa-twitter"></i></li>
+        <li><i class="fa-brands fa-instagram-square"></i></li>
+    </ul>
+</footer>
--- view/headerView.php
+++ view/headerView.php
@@ -1,6 +1,10 @@
 <header>
     <nav>
-        <img src="./public/images/logo.jpg" alt="">
-        <div>blu </div>
+        <img src="./public/images/brand/logo.jpg" alt="verve logo">
+        <ul id="nav-menu">
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Create Your Plan</li>
+        </ul>
     </nav>
 </header>
