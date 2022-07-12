198320e8ff27d9fb55b4116b627b24095d5dafcf
JeongGeon Oh <jeoh0605@gmail.com>
2022-07-13 07:32:17 +0900

    nav features

--- view/headerView.php
+++ view/headerView.php
@@ -1,6 +1,27 @@
 <header>
     <nav>
-        <img src="./public/images/brand/logo.jpg" alt="">
-        <div>header</div>
+        <navbar>
+            <img src="./public/images/brand/logo.jpg" alt="verve logo">
+            <label class="toggle">
+                <input id="hamburger" type="checkbox">
+                <div>
+                    <div>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <svg>
+                        <use xlink:href="#path">
+                    </use></svg>
+                    <svg>
+                        <use xlink:href="#path">
+                    </use></svg>
+                </div>
+            </label>
+            <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                <symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44 44" id="path">
+                    <path d="M22,22 L2,22 C2,11 11,2 22,2 C33,2 42,11 42,22"></path>
+                </symbol>
+            </svg>
+        </navbar>
     </nav>
 </header>
