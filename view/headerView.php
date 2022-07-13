fa3f2e3763c60523b6daf6c878fe49964b2e29e2
JeongGeon Oh <jeoh0605@gmail.com>
2022-07-13 10:22:26 +0900

    header

--- view/headerView.php
+++ view/headerView.php
@@ -1,32 +1,44 @@
 <header>
     <nav>
-        <navbar>
+        <nav-web>
             <img src="./public/images/brand/logo.jpg" alt="verve logo">
-            <label class="toggle">
-                <input id="hamburger" type="checkbox">
-                <div>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Create Your Plan</li>
+            </ul>
+        </nav-web>
+        <nav-mobile>
+            <nav-bar>
+                <img src="./public/images/brand/logo.jpg" alt="verve logo">
+                <label class="toggle">
+                    <input id="hamburger" type="checkbox">
                     <div>
-                        <span></span>
-                        <span></span>
+                        <div>
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <svg>
+                            <use xlink:href="#path">
+                            </use>
+                        </svg>
+                        <svg>
+                            <use xlink:href="#path">
+                            </use>
+                        </svg>
                     </div>
-                    <svg>
-                        <use xlink:href="#path">
-                    </use></svg>
-                    <svg>
-                        <use xlink:href="#path">
-                    </use></svg>
-                </div>
-            </label>
-            <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-                <symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44 44" id="path">
-                    <path d="M22,22 L2,22 C2,11 11,2 22,2 C33,2 42,11 42,22"></path>
-                </symbol>
-            </svg>
-        </navbar>
-        <ul id="nav-menu">
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Create Your Plan</li>
-        </ul>
+                </label>
+                <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                    <symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44 44" id="path">
+                        <path d="M22,22 L2,22 C2,11 11,2 22,2 C33,2 42,11 42,22"></path>
+                    </symbol>
+                </svg>
+            </nav-bar>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Create Your Plan</li>
+            </ul>
+        </nav-mobile>
     </nav>
 </header>
